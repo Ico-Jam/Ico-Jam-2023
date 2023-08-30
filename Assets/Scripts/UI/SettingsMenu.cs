a451f0ba4b1e41d355d27e3984304c626206ef32
Franco Soler <francosolerlanderreche@gmail.com>
2023-08-30 00:40:13 -0300

    Update

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -8,12 +9,23 @@ public class SettingsMenu : MonoBehaviour
     private AudioMixer _audioMixer;
     private PlayerInputActions _uiInputActions;
 
+    [SerializeField]
+    private Slider _masterSlider, _musicSlider, _sfxSlider;
+
     private void Awake()
     {
         _uiInputActions = new PlayerInputActions();
         _uiInputActions.UI.Escape.performed += CloseSettings;
 
         Application.targetFrameRate = 60;
+
+
+        float masterVolume = PlayerPrefs.GetFloat("masterVolume", 1);
+        float musicVolume = PlayerPrefs.GetFloat("musicVolume", 1);
+        float sfxVolume = PlayerPrefs.GetFloat("sfxVolume", 1);
+        _masterSlider.value = masterVolume;
+        _musicSlider.value = musicVolume;
+        _sfxSlider.value = sfxVolume;
     }
 
     private void CloseSettings(InputAction.CallbackContext obj)
